9e7a06a4789a91afe0fabdfb0c5a98b342e5b251
Manu Krishnan T V <tvm@dayscholars.com>
2019-09-03 16:15:06 +0530

    Format Code

--- todo.php
+++ todo.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,26 +13,25 @@
 </head>
 <body class="bg-info">
 <?php
-  include ("connection.php");
-  
-if(isset($_POST["Submit"]))
-{
-  $tname=$_POST["txtTask"];
-  
-  mysqli_query($conn,"INSERT INTO tbltasks(tname,date,status) VALUES('$tname',CURRENT_TIME,0)");
+include "connection.php";
+
+if (isset($_POST["Submit"])) {
+    $tname = $_POST["txtTask"];
+
+    mysqli_query($conn, "INSERT INTO tbltasks(tname,date,status) VALUES('$tname',CURRENT_TIME,0)");
 }
 /*if(isset($_POST["btnTodo"]))
 {
 
-  $tid=$_POST["hiddenField"];
+$tid=$_POST["hiddenField"];
 echo $tid;
-  mysqli_query($conn,"UPDATE tbltasks SET status=1,date=CURRENT_TIME WHERE tid=$tid");
+mysqli_query($conn,"UPDATE tbltasks SET status=1,date=CURRENT_TIME WHERE tid=$tid");
 }
 if(isset($_POST["btnDone"]))
 {
- 
-  $tid=$_POST["hiddenField2"];
-  mysqli_query($conn,"UPDATE tbltasks SET status=0,date=CURRENT_TIME WHERE tid=$tid");
+
+$tid=$_POST["hiddenField2"];
+mysqli_query($conn,"UPDATE tbltasks SET status=0,date=CURRENT_TIME WHERE tid=$tid");
 }*/
 ?>
 <form name="frmTodo" action="todo.php" method="post">
@@ -47,14 +43,14 @@ if(isset($_POST["btnDone"]))
   </div>
   <div class="row">
     <div class="col">
-      
+
         <div class="input-group mb-3">
         <input type="text" name="txtTask" class="form-control" placeholder="Enter task to do..">
         <div class="input-group-append">
         <button class="btn btn-primary" type="submit" name="Submit" id="btnAdd">Add Task</button>
         </div>
         </div>
-     
+
     </div>
   </div>
   <div class="row">
@@ -63,21 +59,19 @@ if(isset($_POST["btnDone"]))
       <ul class="list-group">
 
       <?php
-        $qry2=mysqli_query($conn,"SELECT tid,tname,date,status FROM tbltasks WHERE status=0 ORDER BY date DESC");
-        if(mysqli_num_rows($qry2)>0)
-        {
-	        while($row=mysqli_fetch_array($qry2))
-	        {
-      ?>
+$qry2 = mysqli_query($conn, "SELECT tid,tname,date,status FROM tbltasks WHERE status=0 ORDER BY date DESC");
+if (mysqli_num_rows($qry2) > 0) {
+    while ($row = mysqli_fetch_array($qry2)) {
+        ?>
 
        <li class="list-group-item">
        <div class="d-flex w-100 justify-content-between">
         <h5 class="mb-1"><?php echo $row['tname']; ?></h5>
-        <button class="btn" type="submit" id="btnTodo" name="btnTodo" ><a href="update.php?id=<?php echo $row['tid'];?>"><i class="far fa-check-circle"></i></a></button>
+        <button class="btn" type="submit" id="btnTodo" name="btnTodo" ><a href="update.php?id=<?php echo $row['tid']; ?>"><i class="far fa-check-circle"></i></a></button>
        </div>
         <small class="text-secondary"><?php echo $row['date']; ?></small>
         </li>
-       <?php }} ?>
+       <?php }}?>
       </ul>
     </div>
     <div class="col">
@@ -85,21 +79,19 @@ if(isset($_POST["btnDone"]))
        <ul class="list-group">
 
        <?php
-        $qry3=mysqli_query($conn,"SELECT tid,tname,date,status FROM tbltasks WHERE status=1 ORDER BY date DESC");
-        if(mysqli_num_rows($qry3)>0)
-        {
-	        while($row=mysqli_fetch_array($qry3))
-	        {
-      ?>
+$qry3 = mysqli_query($conn, "SELECT tid,tname,date,status FROM tbltasks WHERE status=1 ORDER BY date DESC");
+if (mysqli_num_rows($qry3) > 0) {
+    while ($row = mysqli_fetch_array($qry3)) {
+        ?>
 
       <li class="list-group-item">
        <div class="d-flex w-100 justify-content-between">
         <h5 class="mb-1"><?php echo $row['tname']; ?></h5>
-        <button class="btn" type="submit" id="btnDone" name="btnDone" ><a href="update2.php?id=<?php echo $row['tid'];?>"><i class="fas fa-check-circle"></i></a></button>
+        <button class="btn" type="submit" id="btnDone" name="btnDone" ><a href="update2.php?id=<?php echo $row['tid']; ?>"><i class="fas fa-check-circle"></i></a></button>
       </div>
         <small class="text-secondary"><?php echo $row['date']; ?></small>
         </li>
-        <?php }} ?>
+        <?php }}?>
         </ul>
     </div>
   </div>
